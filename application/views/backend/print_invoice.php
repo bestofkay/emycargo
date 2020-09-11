f4fb56418cf3dd4c9da5ef2f346e21efc20b2f7c
bestofkay <32548508+bestofkay@users.noreply.github.com>
2020-09-11 16:01:07 +0100

    make it better8

--- application/views/backend/print_invoice.php
+++ application/views/backend/print_invoice.php
@@ -209,11 +209,11 @@ p{font-size: 13px; margin:7px}
                    <th colspan="3" style="border-bottom: 1px solid #fff;"></th>
                    <th colspan="3" style="border-bottom: 1px solid #fff;"><?= number_format($total, 2) ?></th>
                </tr>
-                    <?php $amount_words= convert_number($total); ?>
+                    <?php $amount_words= numberTowords($total); ?>
                <tr>
                    <th colspan="11" class="p_d"style=" border-top: 2px solid #ddd;">
-                       <p>Amount on words:</p>
-                       <p><?= $amount_words.' naira only'; ?></p>
+                       <p>Amount in words:</p>
+                       <p><?= ucwords(strtolower('naira '.$amount_words.' kobo only')); ?></p>
                    </th>
                   
                </tr>
@@ -245,51 +245,87 @@ p{font-size: 13px; margin:7px}
 
 <?php
 
-function convert_number($number) {
-    if (($number < 0) || ($number > 999999999)) {
-        throw new Exception("Number is out of range");
-    }
-    $giga = floor($number / 1000000);
-    // Millions (giga)
-    $number -= $giga * 1000000;
-    $kilo = floor($number / 1000);
-    // Thousands (kilo)
-    $number -= $kilo * 1000;
-    $hecto = floor($number / 100);
-    // Hundreds (hecto)
-    $number -= $hecto * 100;
-    $deca = floor($number / 10);
-    // Tens (deca)
-    $n = $number % 10;
-    // Ones
-    $result = "";
-    if ($giga) {
-        $result .= convert_number($giga) .  "Million";
-    }
-    if ($kilo) {
-        $result .= (empty($result) ? "" : " ") .convert_number($kilo) . " Thousand";
-    }
-    if ($hecto) {
-        $result .= (empty($result) ? "" : " ") .convert_number($hecto) . " Hundred";
-    }
-    $ones = array("", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eightteen", "Nineteen");
-    $tens = array("", "", "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eigthy", "Ninety");
-    if ($deca || $n) {
-        if (!empty($result)) {
-            $result .= " and ";
-        }
-        if ($deca < 2) {
-            $result .= $ones[$deca * 10 + $n];
-        } else {
-            $result .= $tens[$deca];
-            if ($n) {
-                $result .= "-" . $ones[$n];
-            }
-        }
-    }
-    if (empty($result)) {
-        $result = "zero";
-    }
-    return $result;
+function numberTowords($num)
+{
+
+$ones = array(
+0 =>"ZERO",
+1 => "ONE",
+2 => "TWO",
+3 => "THREE",
+4 => "FOUR",
+5 => "FIVE",
+6 => "SIX",
+7 => "SEVEN",
+8 => "EIGHT",
+9 => "NINE",
+10 => "TEN",
+11 => "ELEVEN",
+12 => "TWELVE",
+13 => "THIRTEEN",
+14 => "FOURTEEN",
+15 => "FIFTEEN",
+16 => "SIXTEEN",
+17 => "SEVENTEEN",
+18 => "EIGHTEEN",
+19 => "NINETEEN",
+"014" => "FOURTEEN"
+);
+$tens = array( 
+0 => "ZERO",
+1 => "TEN",
+2 => "TWENTY",
+3 => "THIRTY", 
+4 => "FORTY", 
+5 => "FIFTY", 
+6 => "SIXTY", 
+7 => "SEVENTY", 
+8 => "EIGHTY", 
+9 => "NINETY" 
+); 
+$hundreds = array( 
+"HUNDRED", 
+"THOUSAND", 
+"MILLION", 
+"BILLION", 
+"TRILLION", 
+"QUARDRILLION" 
+); /*limit t quadrillion */
+$num = number_format($num,2,".",","); 
+$num_arr = explode(".",$num); 
+$wholenum = $num_arr[0]; 
+$decnum = $num_arr[1]; 
+$whole_arr = array_reverse(explode(",",$wholenum)); 
+krsort($whole_arr,1); 
+$rettxt = ""; 
+foreach($whole_arr as $key => $i){
+	
+while(substr($i,0,1)=="0")
+		$i=substr($i,1,5);
+if($i < 20){ 
+/* echo "getting:".$i; */
+$rettxt .= $ones[$i]; 
+}elseif($i < 100){ 
+if(substr($i,0,1)!="0")  $rettxt .= $tens[substr($i,0,1)]; 
+if(substr($i,1,1)!="0") $rettxt .= " ".$ones[substr($i,1,1)]; 
+}else{ 
+if(substr($i,0,1)!="0") $rettxt .= $ones[substr($i,0,1)]." ".$hundreds[0]; 
+if(substr($i,1,1)!="0")$rettxt .= " ".$tens[substr($i,1,1)]; 
+if(substr($i,2,1)!="0")$rettxt .= " ".$ones[substr($i,2,1)]; 
+} 
+if($key > 0){ 
+$rettxt .= " ".$hundreds[$key]." "; 
+}
+} 
+if($decnum > 0){
+$rettxt .= " and ";
+if($decnum < 20){
+$rettxt .= $ones[$decnum];
+}elseif($decnum < 100){
+$rettxt .= $tens[substr($decnum,0,1)];
+$rettxt .= " ".$ones[substr($decnum,1,1)];
+}
+}
+return $rettxt;
 }
 ?>
